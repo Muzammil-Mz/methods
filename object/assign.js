5cf9e9739aa5cb8d4fe046519416c47e91727494
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 18:58:06 +0000

    09

---
+++ object/assign.js
@@ -0,0 +1,17 @@
+//object.assign.(object name) allows you to assign the key value from 1 object to another object
+
+// let students={
+//     name:"kishore",
+//     class:"mbbs",
+//     age:23
+// }
+
+
+// let teachers={
+//     subject:"pharma",
+//     age:55,
+//     gender:"male"
+// }
+
+// console.log(Object.assign(students,teachers)); //object teachers data is assigned to students data
+// console.log(Object.assign(students,teachers)); //object students data is assigned to teachers data
