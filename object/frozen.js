91c6c9a570d7ee5c1b828212bc3d45731d0bcfba
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:10:18 +0000

    09

---
+++ object/frozen.js
@@ -0,0 +1,8 @@
+//object.isfrozen checks whether object frozen or not if freeze true if not false
+// let students={
+//         name:"kishore",
+//         class:"mbbs",
+//         age:23
+//     }
+// Object.freeze(students)
+// console.log(Object.isFrozen(students));
