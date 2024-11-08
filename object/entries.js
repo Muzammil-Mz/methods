8e0c1f97a69fa6cbe9c4f53ad818bdbc9baaed86
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:05:11 +0000

    09

---
+++ object/entries.js
@@ -0,0 +1,8 @@
+// Object.entries(objectname) gives object data in the form of arrays
+// let students={
+//     name:"kishore",
+//     class:"mbbs",
+//     age:23
+// }
+
+// console.log(Object.entries(students))
