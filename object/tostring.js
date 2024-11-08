3f044b0243d98e275b348faf99f02c1c1d50acb2
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:19:24 +0000

    09

---
+++ object/tostring.js
@@ -0,0 +1,7 @@
+//object.tostring(obj name ) converts given number in to various number system hexa,decimal,binary
+//2 for binary 10-decimal 8 octa 16hexadec
+
+
+// let givennumber=15
+// let check=givennumber.toString(2)  //2 is binary
+// console.log(check);
