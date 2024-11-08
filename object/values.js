9a5c140bfd68a96465fadab4e9f4699370e1f019
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:19:41 +0000

    09

--- object/values.js
+++ object/values.js
@@ -1,8 +1,8 @@
 //object.keys(objname) gives obj values 
 
-let students={
-    name:"kishore",
-    class:"mbbs",
-    age:23
-}
-console.log(Object.values(students));
+// let students={
+//     name:"kishore",
+//     class:"mbbs",
+//     age:23
+// }
+// console.log(Object.values(students));
