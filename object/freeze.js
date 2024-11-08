f36abc9e6af8012ec042c965b6c4285cf67de68e
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:08:09 +0000

    09

---
+++ object/freeze.js
@@ -0,0 +1,10 @@
+//object.freeze(objectname) freezes the object from add,del update
+// let students={
+//     name:"kishore",
+//     class:"mbbs",
+//     age:23
+// }
+
+// students.name="jd" //tried to update name but .freeze prevents
+// Object.freeze(students) //this line prevents from update
+// console.log(students);
