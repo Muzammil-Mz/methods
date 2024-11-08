2466f94ec681557f2d0b22c867042bd5780d7396
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-08 19:15:40 +0000

    09

---
+++ object/seal.js
@@ -0,0 +1,10 @@
+//object.seal(objname) adds values to existing values in object
+
+// let students={
+//     name:"kishore",
+//     class:"mbbs",
+//     age:23
+// }
+// Object.seal(students)
+// students.name="nasir"
+// console.log(Object.seal(students));
