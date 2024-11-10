3b21decb6429579cb7269d32660360bb6aae0bf7
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:45:54 +0000

    10

--- String/padding.js
+++ String/padding.js
@@ -3,3 +3,7 @@
 // let game=text.padEnd(10,"!") //here 10 is the number of times to pad and  ! is item to be padded 
 // console.log(game);
 
+// 13.The padEnd() method pads a string from the start - pading nothing but addition of extra material pattern or anything
+// let text="hello"
+// let game=text.padEnd(10,"!") //here 10 is the number of times to pad and  ! is item to be padded 
+// console.log(game);
