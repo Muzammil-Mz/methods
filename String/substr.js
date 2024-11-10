d9506050bd609c77b6890d3fc654e72b04ba93b8
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:48:57 +0000

    10

---
+++ String/substr.js
@@ -0,0 +1,8 @@
+// 6.substr(index,length) -- extracts from the specifeied index and length from their eg
+// let text = "HELLO WORLD";
+// let char = text.substr(1,8); //extracted from 1 to 8 9 included
+// console.log(char);
+
+// let text = "HELLO WORLD";
+// let char = text.substring(1,8);
+// console.log(char); //extracted fromm 1 to 8 but, 8 excluded
