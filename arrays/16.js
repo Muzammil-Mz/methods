0d9ccf3949243939ef40030a8c681a5982759d43
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 15:23:21 +0000

    10

---
+++ arrays/16.js
@@ -0,0 +1,13 @@
+// Array copyWithIn()
+// Copy the elememnt from 1 index to another index
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(arr.copyWithin(0, 2, 3));
+
+
+let words = ["apple", "ball", "cat", "dog"];
+
+console.log(words.copyWithin(1, 2, 3));
+
+console.log(words);
---
+++ arrays/18.js
@@ -0,0 +1,10 @@
+// Array fill() Method
+// It fills the all the elements with the given parameter
+// It also takes 3 parameters
+// Target , start , end
+// It Changes the Original Array
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr);
+
+console.log(arr.fill(20, 4, 9));
+console.log(arr);
