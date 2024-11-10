147941bcadd21f407040144737baa41ffd4c6710
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:47:32 +0000

    10

--- String/trim.js
+++ String/trim.js
@@ -1,4 +1,20 @@
-// 12.trimstart() -- remves white spaces from start only
+// 10.trim()
+// let text = "   HELLO WORLD          ";
+// let char = text.trim(); //removes empty spaces from string
+// console.log(text);v
+
+
+
+
+
+// 12.trimEnd() -- remves white spaces from start only
 // let text = " HELLO WORLD   8";
 // let char = text.trimEnd(); //
 // console.log(char);
+
+// 11.trimstart() -- remves white spaces from start only
+// let text = "  HELLO WORLD";
+// let char = text.trimStart(); //
+// console.log(char);
+
+
