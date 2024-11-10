58a9559083074946c7a41631dc13fa020a7d581a
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:50:10 +0000

    10

---
+++ String/slice.js
@@ -0,0 +1,13 @@
+// 4.Slice - slices from mentioned number to end of string
+// let text="hello world"
+// let text2=text.slice(2)
+// console.log(text2); //llo world
+
+
+// let text="hello world"
+// let text2=text.slice(2,7)
+// console.log(text2); //gives output from start to of specifed number
+
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12, -6);- banana
