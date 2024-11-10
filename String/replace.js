18355a87bc50f40841f58c3d8806e1204989ff49
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:43:41 +0000

    10

--- String/replace.js
+++ String/replace.js
@@ -2,3 +2,13 @@
 // let text="Hello World Hello"
 // let text2=text.replaceAll(/Hello/g,"hi")
 // console.log(text2);
+
+
+//replace(/string/i) changes only first 
+// let text="Hello World Hello"
+// let text2=text.replace(/hello/i,"hi")  //i is insensitive char here Hello is orig and hello is given
+// console.log(text2);
+
+// let text="Hello World Hello"
+// let text2=text.replace(/Hello/g,"hi")  //g replaces all characters matching bt case sensit
+// console.log(text2);
