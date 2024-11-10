084e9ebd9536241830d8d96a9b1f89f686efb5ea
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:48:20 +0000

    10

---
+++ String/lowercase.js
@@ -0,0 +1,9 @@
+// 7.toLowerCase() -- converts string to lowercase
+// let text="HELLO WORLD";
+// let char = text.toLowerCase();
+// console.log(char)
+
+// 8.toUpperCase(); - to upper case
+// let text="hello world";
+// let char = text.toUpperCase();
+// console.log(char)
