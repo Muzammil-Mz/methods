caa90959cca1b52176ce75464e5ba718d5d62266
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:49:48 +0000

    10

---
+++ String/substring.js
@@ -0,0 +1,4 @@
+// 5.The substring() method extract a part of a string and returns the extracted parts in a new string
+// let text = "HELLO WORLD";
+// let char = text.substring(2);
+// console.log(char);
