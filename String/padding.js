9d1eb8a9f411e682156b0fc55e57e440c41f1bac
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-10 14:45:00 +0000

    10

---
+++ String/padding.js
@@ -0,0 +1,5 @@
+// 14.The padStart() method pads a string from the start - pading nothing but addition of extra material pattern or anything
+// let text="hello"
+// let game=text.padEnd(10,"!") //here 10 is the number of times to pad and  ! is item to be padded 
+// console.log(game);
+
